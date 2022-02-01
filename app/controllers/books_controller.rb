f4947f98934addecf061423d19e1810b7d32be60
MilkMilkMilkMilkMilkMilk <izumilk0922@gmail.com>
2022-02-01 12:07:44 +0000

    [Add] viewファイル

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,11 @@
 class BooksController < ApplicationController
-  def new
-  end
-  
-  def create
-  end
-  
   def index
   end
   
   def show
-  end
+    
   
   def edit
   end
   
-  def update
-  end
-  
-  def destroy
-  end
 end
