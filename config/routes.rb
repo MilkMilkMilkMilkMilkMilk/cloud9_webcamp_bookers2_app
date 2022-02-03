aede60137cb87cb81f16ad68812c719974d197d8
MilkMilkMilkMilkMilkMilk <izumilk0922@gmail.com>
2022-02-03 12:54:27 +0000

    [Add] ルーティングの記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/top"=>'homes#top'
+  get "home/about"=>"homes#about",as: :homes
+
   resources :books
+  resources :users
 end
