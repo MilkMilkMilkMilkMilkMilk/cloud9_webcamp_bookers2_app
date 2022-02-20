b79bc61292c86093f53d7f24049d0931890c7d35
MilkMilkMilkMilkMilkMilk <izumilk0922@gmail.com>
2022-02-20 07:20:20 +0000

    [Finish] 投稿詳細ページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,8 +13,9 @@ class BooksController < ApplicationController
   end
 
   def show
+    @books = Book.new
     @book = Book.find(params[:id])
-    @user = User.find(params[:id])
+    @user = User.find(@book.user_id)
   end
 
   def edit
