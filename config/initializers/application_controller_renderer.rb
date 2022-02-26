83fa7cce89f14cff49a8b9f9c62b95309606b9c9
MilkMilkMilkMilkMilkMilk <izumilk0922@gmail.com>
2022-02-26 11:23:31 +0000

    [Finish] Bookers2_ALL

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '3ae926a739a682c0c072003a5a0001f923c9496860a32f5a9f2425fa8cf9df102a04e207237245b12e4dffb3d044da78809f6c1fcdcbeb1e369c2555ccd5a56c'
